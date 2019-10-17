48858052c18d7624b8475fc49c47f248aeae8290
Ashby Creighton Crum <acreighton1@dmacc.edu>
2019-10-16 21:48:22 -0700

    Assignment 8.1

---
+++ CIS174_TestCoreApp-master/Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/HouseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class HouseViewModel
+    {
+        public int Id { get; set; }
+        public int Bedrooms { get; set; }
+        public int SquareFeet { get; set; }
+        public DateTime DateBuilt { get; set; }
+        public string Flooring { get; set; }
+        
+    }
+}
